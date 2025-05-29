62f55dfe383d4e593afaf70049529d5cf046d424
fmirkowski <mirkowskifranciszek@gmail.com>
2025-05-29 13:21:51 +0200

    adapted reward computation to account for std =0

--- rl/grpo.py
+++ rl/grpo.py
@@ -46,7 +46,7 @@ class GRPOTtrainer:
         mean_reward = torch.mean(rewards)
         std_reward = torch.std(rewards)
         # Standardize rewards using vectorized operations
-        advantages = (rewards - mean_reward) / std_reward
+        advantages = (rewards - mean_reward) / std_reward if std_reward > 0 else rewards
         print(std_reward)
 
         pass
